2d094caa14b97c3e6b129b5088df5408988e38a5
sarun tapee <tpzrun@gmail.com>
2025-10-07 11:07:04 -0500

    fix: di

--- app/src/main/java/edu/miu/midterm/ui/ImageExpViewModel.kt
+++ app/src/main/java/edu/miu/midterm/ui/ImageExpViewModel.kt
@@ -1,6 +1,7 @@
 package edu.miu.midterm.ui
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.miu.midterm.data.ImageRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -8,6 +9,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
+@HiltViewModel
 class ImageExpViewModel @Inject constructor(
     private val imageRepository: ImageRepository
 ): ViewModel()  {
